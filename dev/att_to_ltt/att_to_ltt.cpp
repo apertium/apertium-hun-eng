cf3a13454585c5cef249b2749bc8e51bbcc5b1a3
savior42 <savior42@users.sourceforge.net>
2013-07-05 08:06:50 +0000

    Infinite loop removed.
    M    att_to_ltt.cpp

--- dev/att_to_ltt/att_to_ltt.cpp
+++ dev/att_to_ltt/att_to_ltt.cpp
@@ -42,6 +42,8 @@ public:
       iss.str(line);
       int from, to;
       wstring upper, lower;
+      bool new_from = false;
+      bool new_to   = false;
 
       if (!(iss >> from)) {
         continue;
@@ -53,31 +55,35 @@ public:
         first_line = false;
       }
 
+      /* Is the source state new? */
+      new_from = corr.find(from) == corr.end();
       /* Get the id of the state from in the transducer we are building. */
-      map<int, int>::iterator it = corr.find(from);
-      if (it == corr.end()) {
-        /* First time we see this id: register it. */
-        corr[from] = transducer.size();
-      } else {
-        /* This id already has a mapping: use the id in the transducer. */
-  //      from = it->second;
-      }
-
-      from = corr[from];
 
       /* Final state. */
       if (!(iss >> to >> upper >> lower)) {
+        if (new_from) {
+          corr[from] = transducer.size();
+        }
+        from = corr[from];
         transducer.setFinal(from);
+        // TODO: what if from is new?
       } else {
         convert_hfst(upper);
         convert_hfst(lower);
         int tag = alphabet(symbol_code(upper), symbol_code(lower));
 
+        /* Is the source state new? */
+        new_to = corr.find(to) == corr.end();
+
+        if (new_from) {
+          corr[from] = transducer.size() + (new_to ? 1 : 0);
+        }
+        from = corr[from];
+
         /* Now with the target state: */
-        map<int, int>::iterator it_to = corr.find(to);
-        if (it_to != corr.end()) {
+        if (!new_to) {
           /* We already know it, possibly by a different name: link them! */
-          to = it_to->second;
+          to = corr[to];
           transducer.linkStates(from, to, tag);
         } else {
           /* We haven't seen it yet: add a new state! */
